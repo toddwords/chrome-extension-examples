d51b92a7112e7623e3bf96c6615f7852c1123f06
toddwords <toddwords@gmail.com>
2021-10-22 17:39:47 -0400

    party city

---
+++ partycity/inject.js
@@ -0,0 +1,7 @@
+//This is the code that will be injected on any page
+setTimeout(function(){
+  $('.image-quad-holder-bottom img').remove()
+  $('.image-quad-holder-bottom h3').text("I am a spooky ghost BWEH")
+  $('.link-farm-transparent-block-147618-1--1 a').text("BOO")
+  $('.product-title a:contains("Peanut Butter Cups")').parent().parent().find(".price-container span").text("$20000")
+}, 1000)
