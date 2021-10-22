d0fbbb60ab617fb7ee49aabdb20631bd3e007c61
toddwords <toddwords@gmail.com>
2021-10-22 17:40:58 -0400

    update

--- partycity/inject.js
+++ partycity/inject.js
@@ -4,4 +4,5 @@ setTimeout(function(){
   $('.image-quad-holder-bottom h3').text("I am a spooky ghost BWEH")
   $('.link-farm-transparent-block-147618-1--1 a').text("BOO")
   $('.product-title a:contains("Peanut Butter Cups")').parent().parent().find(".price-container span").text("$20000")
+  //$('img').attr('src', newimageURL)
 }, 1000)
